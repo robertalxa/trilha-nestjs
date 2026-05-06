ddb9f793aa80d44b2f2754dcb1d8fa71443e6a36
Robert Alexandre <robert.alexandrea7@gmail.com>
2026-05-06 19:00:40 -0300

    Finalizando o CRUD

--- adopet/src/controller/PetController.ts
+++ adopet/src/controller/PetController.ts
@@ -10,4 +10,23 @@ export default class PetController {
     listaDePets.push(novoPet);
     return res.status(201).send(novoPet);
   }
+
+  listaPets(req: Request, res: Response) {
+    return res.status(200).json(listaDePets);
+  }
+
+  atualizaPet(req: Request, res: Response) {
+    const { id } = req.params;
+    const { adotado, especie, idade, nome } = req.body as TipoPet;
+    const pet = listaDePets.find((pet) => pet.id === Number(id));
+    if (!pet) {
+      return res.status(404).json({ erro: "Pet não encontrado" });
+    }
+
+    pet.nome = nome;
+    pet.idade = idade;
+    pet.especie = especie;
+    pet.adotado = adotado;
+    return res.status(200).json(pet);
+  }
 }
