c85a273e91400423725f3c5c31390652619f9707
Robert Alexandre <robert.alexandrea7@gmail.com>
2026-05-06 18:41:47 -0300

    Ajustando tipos e determinando pasta de output do build

--- adopet/src/app.ts
+++ adopet/src/app.ts
@@ -1,13 +1,19 @@
-import express from "express";
+import express, { Response } from "express";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (_, res) => {
+app.get("/", (_, res: Response) => {
   res.send("Bem vindo ao curso de TypeScript!");
 });
 
-function criaPet(id, nome, especie, idade, adotado) {
+function criaPet(
+  id: number,
+  nome: string,
+  especie: string,
+  idade: number,
+  adotado: boolean,
+) {
   return {
     id,
     nome,
@@ -23,7 +29,7 @@ function geraId() {
   return id;
 }
 
-app.post("/pets", (_, res) => {
+app.post("/pets", (_, res: Response) => {
   const pet1 = criaPet(geraId(), "Bolt", "cachorro", 3, false);
   const pet2 = criaPet(geraId(), "Mel", "gato", 2, false);
 
