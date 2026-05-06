c936878baa1c4342783e9a850f82671d4b8fb5d1
Robert Alexandre <robert.alexandrea7@gmail.com>
2026-05-06 18:57:41 -0300

    Criando tipo pet e utilizando para receber corretamente as informações do body

--- adopet/src/controller/PetController.ts
+++ adopet/src/controller/PetController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
+import { TipoPet } from "../tipos/tipoPet";
 
-let listaDePets = [];
+let listaDePets: TipoPet[] = [];
 
 export default class PetController {
   criaPet(req: Request, res: Response) {
-    const novoPet = req.body;
+    const { id, adotado, nome, especie, idade } = <TipoPet>req.body;
+    const novoPet = { id, adotado, nome, especie, idade };
     listaDePets.push(novoPet);
     return res.status(201).send(novoPet);
   }
---
+++ adopet/src/tipos/TipoPet.ts
@@ -0,0 +1,7 @@
+export type TipoPet = {
+  id: number;
+  nome: string;
+  especie: string;
+  adotado: boolean;
+  idade: number;
+};
