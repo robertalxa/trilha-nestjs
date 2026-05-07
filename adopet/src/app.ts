c08311b450bedb64f0b1610e3a046490aacabaf3
Robert Alexandre <robert.alexandrea7@gmail.com>
2026-05-07 15:24:16 -0300

    Incluindo typeorm e banco de dados sqlite

--- adopet/src/app.ts
+++ adopet/src/app.ts
@@ -1,8 +1,18 @@
+import { AppDataSource } from "./config/dataSource";
 import express, { Response } from "express";
 import router from "./routes";
+import "reflect-metadata";
 
 const app = express();
 app.use(express.json());
 router(app);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Banco de dados conectado");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 export default app;
