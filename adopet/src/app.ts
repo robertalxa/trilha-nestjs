17080bda551f61f86fd7b4e9e9a41d309ed949fa
Robert Alexandre <robert.alexandrea7@gmail.com>
2026-05-11 21:27:12 -0300

    Lidando melhor com erros internos através de um middleware personalizado que não deixa a aplicação quebrar

--- adopet/src/app.ts
+++ adopet/src/app.ts
@@ -1,11 +1,14 @@
 import { AppDataSource } from "./config/dataSource";
+import "express-async-errors";
 import express, { Response } from "express";
 import router from "./routes";
 import "reflect-metadata";
+import { erroMiddleware } from "./middleware/erro";
 
 const app = express();
 app.use(express.json());
 router(app);
+app.use(erroMiddleware);
 
 AppDataSource.initialize()
   .then(() => {
